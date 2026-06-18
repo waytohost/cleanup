8665cd2cc8896e96ddce34d8145f6c44b690866d
HARY JOHNS <haryjohns@hotmail.com>
2026-06-19 00:28:38 +0530

    Update log_cleanup.sh

--- log_cleanup.sh
+++ log_cleanup.sh
@@ -59,6 +59,17 @@ find /var/log/imunify360 -type f -name "*.log*" -exec truncate -s 0 {} \;
 find /var/log/imunify360-webshield -type f -name "*.log" -exec truncate -s 0 {} \;
 
 echo ""
+echo "===== bitninja ====="
+
+echo "Removing compressed imunify360 logs..."
+find /var/log/bitninja -type f -name "*.gz" -print -delete > /dev/null
+
+echo "Clearing bitninja..."
+find /var/log/bitninja -type f -name "*.log" -exec truncate -s 0 {} \;
+find /var/log/bitninja -type f -name "*.log*" -exec truncate -s 0 {} \;
+
+
+echo ""
 echo "===== PHP Sessions ====="
 
 echo "Deleting PHP session files older than 120 minutes..."
